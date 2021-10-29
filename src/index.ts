4745e4bb87475fbbfcebb062b110576ff8d46483
depthbomb <reesepup77@hotmail.com>
2021-10-28 20:36:38 -0500

    Add functions to retrieve first ID from input

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,41 @@ const userMentionPattern = /<@!?(\d+)>/gm;
 const roleMentionPattern = /<@&(\d+)>/gm;
 const channelMentionPattern = /<#(\d+)>/gm;
 
-function _extractIds(iterator: IterableIterator<RegExpMatchArray>): string | string[] | null {
+function _extractIds(iterator: IterableIterator<RegExpMatchArray>): string[] | null {
 	const matches = [...iterator];
 	if (!matches.length) return null;
-	if (matches.length > 1) {
-		return matches.map(m => m[1]);
-	} else {
-		return matches[0][1];
-	}
+	return matches.map(m => m[1]);
+};
+
+/**
+ * Returns the first user ID from a mention in a string
+ * @param string Input containing user mentions
+ * @returns An ID or null if none were found
+ */
+export function getUserIdFromMention(string: string): string | null {
+	const ids = getUserIdsFromMention(string);
+	if (ids && ids.length) return ids[0];
+	return null;
 };
 
 /**
  * Extracts user IDs from a string
  * @param string Input containing user mentions
- * @returns A single ID if only one ID was found, an array of IDs if multiple were found, or null if none were found
+ * @returns An array of IDs or null if none were found
  */
 export function getUserIdsFromMention(string: string): string | string[] | null {
 	return _extractIds(string.matchAll(userMentionPattern));
+};
+
+/**
+ * Returns the first role ID from a mention in a string
+ * @param string Input containing role mentions
+ * @returns An ID or null if none were found
+ */
+export function getRoleIdFromMention(string: string): string | null {
+	const ids = getRoleIdsFromMention(string);
+	if (ids && ids.length) return ids[0];
+	return null;
 };
 
 /**
@@ -28,6 +46,17 @@ export function getUserIdsFromMention(string: string): string | string[] | null 
  */
 export function getRoleIdsFromMention(string: string): string | string[] | null {
 	return _extractIds(string.matchAll(roleMentionPattern));
+};
+
+/**
+ * Returns the first role ID from a mention in a string
+ * @param string Input containing role mentions
+ * @returns An ID or null if none were found
+ */
+export function getChannelIdFromMention(string: string): string | null {
+	const ids = getChannelIdsFromMention(string);
+	if (ids && ids.length) return ids[0];
+	return null;
 };
 
 /**
